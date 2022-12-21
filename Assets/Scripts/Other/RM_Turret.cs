bf64c35853ca6b564b5844ccfdaf05326db78c26
Jens Heukers <jens.heukers@hotmail.com>
2022-12-21 14:24:13 +0100

    Work on turret

---
+++ Assets/Scripts/Other/RM_Turret.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RM_Turret : MonoBehaviour {
+
+    [SerializeField]
+    private Transform mount;
+
+    [SerializeField]
+    private float mountRotateSpeed = 250f;
+
+    [SerializeField]
+    private Transform gun;
+
+
+    [SerializeField]
+    private float gunRotateSpeed = 250f;
+
+    [SerializeField]
+    private float minGunRotateAmount = -40f;
+
+    [SerializeField]
+    private float maxGunRotateAmount = 75f;
+
+[SerializeField]
+    private Transform target;
+
+    private void Update() {
+        if (target) {
+            //First part - Rotates the base of the turret
+            Quaternion lookRotation = Quaternion.LookRotation(target.position - mount.position, Vector3.up);
+            mount.rotation = Quaternion.RotateTowards(mount.rotation, lookRotation, Time.deltaTime * mountRotateSpeed);
+            mount.localEulerAngles = new Vector3(0, 0, mount.localEulerAngles.z);
+
+            Quaternion gunlookRotation = Quaternion.LookRotation(target.position - mount.position, Vector3.up) * Quaternion.AngleAxis(-90, Vector3.right);
+            gun.rotation = Quaternion.RotateTowards(gun.rotation, gunlookRotation, Time.deltaTime * gunRotateSpeed);
+
+            gun.localEulerAngles = new Vector3(gun.localEulerAngles.x, 0, 0);
+
+        }
+    }
+
+    public void SetTarget(Transform target) {
+        this.target = target;
+    }
+}
