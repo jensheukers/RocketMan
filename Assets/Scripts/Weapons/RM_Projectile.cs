aee0c85a7eb823783efc08bdb1e9bef28817cd50
Jens Heukers <jens.heukers@hotmail.com>
2023-01-16 20:09:14 +0100

    Work on assets, added blast radius to projectiles and blastback on rigidbodies

--- Assets/Scripts/Weapons/RM_Projectile.cs
+++ Assets/Scripts/Weapons/RM_Projectile.cs
@@ -9,13 +9,31 @@ public class RM_Projectile : MonoBehaviour {
     [Range(0f, 100f)]
     public int damage = 100; /** The amount of damage to inflict on a RM_HealthComponent */
 
+    public float blastRadius = 0;
+
     public void OnCollisionEnter(Collision collision) {
         GameObject particleSystem = Instantiate(hitParticlePrefab, transform.position, transform.rotation);
 
         if (collision.gameObject.GetComponent<RM_HealthComponent>()) {
             collision.gameObject.GetComponent<RM_HealthComponent>().Damage(damage);
         }
+        else if (collision.gameObject.GetComponent<Rigidbody>()) {
+            //Blast back any objects without healthcomponent but do have a rigidbody
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(GetComponent<Rigidbody>().velocity);
+        }
+        else {
+            //Check for targets in blastradius
+            if (blastRadius > 0) {
+                Collider[] colliders = Physics.OverlapSphere(transform.position, blastRadius);
 
+                foreach (Collider c in colliders) {
+                    if (c.gameObject.GetComponent<RM_HealthComponent>()) {
+                        c.gameObject.GetComponent<RM_HealthComponent>().Damage(damage);
+                    }
+                }
+            }
+        }
+        
         Destroy(this.gameObject);
     }
 
